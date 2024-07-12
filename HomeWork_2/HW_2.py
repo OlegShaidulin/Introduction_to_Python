f493e2c33502b831a321858f85ad04c5ec14868e
Oleg Shaidulin <olegschaid@gmail.com>
2024-07-12 13:14:14 +0300

    2'nd homework

---
+++ HomeWork_2/HW_2.py
@@ -0,0 +1,5 @@
+count_home_works = 12
+count_hour = 1.5
+course_name = 'Python'
+time_for_one_lesson = 1.5/12
+print(f'Курс: {course_name} всего задач: {count_home_works}, затрачено {count_hour} часов, время на одно занятие {time_for_one_lesson} часа')
