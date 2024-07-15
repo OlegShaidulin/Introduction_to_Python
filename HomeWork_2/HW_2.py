87a22f3b102bc5dfac380db2ae19f94b24efa578
Oleg Shaidulin <olegschaid@gmail.com>
2024-07-15 11:45:03 +0300

    2'nd homework rework

--- HomeWork_2/HW_2.py
+++ HomeWork_2/HW_2.py
@@ -1,5 +1,5 @@
 count_home_works = 12
 count_hour = 1.5
 course_name = 'Python'
-time_for_one_lesson = 1.5/12
+time_for_one_lesson = count_hour/count_home_works
 print(f'Курс: {course_name} всего задач: {count_home_works}, затрачено {count_hour} часов, время на одно занятие {time_for_one_lesson} часа')
