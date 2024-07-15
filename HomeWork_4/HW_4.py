514e26fc79fb2842a07f77eaac1ae2ec4dc982e6
Oleg Shaidulin <olegschaid@gmail.com>
2024-07-15 13:40:58 +0300

    4'rd homework

---
+++ HomeWork_4/HW_4.py
@@ -0,0 +1,6 @@
+my_string = input("Введите предложение: ")
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ","...")) # три точки отображают удаление пробелов
+print(my_string[0])
+print(my_string[-1])
