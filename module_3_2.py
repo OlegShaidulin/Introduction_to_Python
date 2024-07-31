3eec5c3c5de694aef45999568225122209a335fc
Oleg Shaidulin <olegschaid@gmail.com>
2024-07-31 19:32:22 +0300

    homework rework  module_3_2

--- module_3_2.py
+++ module_3_2.py
@@ -11,14 +11,16 @@ def is_vaild_email(email):
 def send_email(message, sender, recipient='university.help@gmail.com'):
     if not is_vaild_email(recipient) == False or not is_vaild_email(sender) == False:
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
-    elif sender == recipient:
+    elif str(sender) == str(recipient):
         print('Нельзя отправить письмо самому себе!')  
-    elif sender == 'university.help@gmail.com':
-        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+    elif recipient != 'university.help@gmail.com':
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
     else:
-        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <sender> на адрес <recipient>.')
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+        
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', 'urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', 'urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru','urban.teacher@mail.ru')
+send_email('Стандартное письмо!','urban.teacher@mail.ru', recipient='university.help@gmail.com' )
