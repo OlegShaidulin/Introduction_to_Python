5dc229de9cf8ee3a51f08f4b2d5b57c642ab4631
Oleg Shaidulin <olegschaid@gmail.com>
2024-08-07 12:50:13 +0300

    rework module_3_4

--- module_3_4.py
+++ module_3_4.py
@@ -1,7 +1,7 @@
 def single_root_words(root_word,*other_words):
     same_words = []
     for word in other_words:
-        if root_word in word or word in root_word:
+        if root_word.lower() in word.lower() or word.lower() in root_word.lower():
             same_words.append(word)
     return same_words
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
