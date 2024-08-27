a2ab389b0cfadac1346b4b05aef85f484369bf6c
Oleg Shaidulin <olegschaid@gmail.com>
2024-08-27 18:28:07 +0300

    homework  module_5_1

---
+++ module_5_1.py
@@ -0,0 +1,19 @@
+class House:
+    def __init__(self, name, stage):
+        self.name = name
+        self.numbers_of_floors = stage
+        
+    def go_to(self, new_floor: int):
+        if new_floor < 1 and new_floor > self.numbers_of_floors:
+            print('There is not such floor')
+        else:
+            for f in range(1, new_floor+1, -1):
+                print(f)
+
+def main():
+    h1 = House('ЖК Горский', 18)
+    h2 = House('Домик в деревне', 2)
+    h1.go_to(5)
+    h2.go_to(10)
+if __name__ == '__main__':
+    main()
